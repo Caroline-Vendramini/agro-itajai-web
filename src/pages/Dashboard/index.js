1c4f63ec85b11b69dd5dc309eda47c132241cca2
Caroline Vendramini <caroline.vendramini07@gmail.com>
2024-12-06 22:33:41 -0300

    adicionando rotas de login e dashboard

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,11 @@
+
+function Dashboard() {
+    return (
+      <div>
+        <h1>Pagina Deshboard</h1>
+      </div>
+    );
+  }
+  
+  export default Dashboard;
+  
---
+++ src/pages/Login/index.js
@@ -0,0 +1,11 @@
+
+function Login() {
+    return (
+      <div>
+        <h1>Bem vindo a pagina Login</h1>
+      </div>
+    );
+  }
+  
+  export default Login;
+  
