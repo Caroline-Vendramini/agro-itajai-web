8c9af019e5777b2b8234ab1c87d0f9e8dee6fd65
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2024-12-08 03:29:33 -0300

    Adiciona Typography

---
+++ src/components/typography/Typography.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Typography.css';
+
+const variants = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'span'];
+
+// Variants: h1, h2, h3, h4, h5, h6, p, span
+const Typography = ({ variant, children, className, ...props }) => {
+  const Component = variants.includes(variant) ? variant : 'span';
+  return (
+    <Component className={`typography ${variant} ${className}`} {...props}>
+      {children}
+    </Component>
+  );
+};
+
+export default Typography;
