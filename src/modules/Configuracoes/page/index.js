05382e868658a5a068bc6a10afa52e2c0a204d8c
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2024-12-15 00:56:33 -0300

    Add configurações

--- src/modules/Configuracoes/page/index.js
+++ src/modules/Configuracoes/page/index.js
@@ -1,8 +1,32 @@
 
+import Button from "../../../components/button/Button";
+import Input from "../../../components/input/Input";
+import Modal from "../../../components/modal/Modal";
+import Typography from "../../../components/typography/Typography";
+import "./index.css";
+
 function Configuracoes() {
+  // TODO: adicionar funcionalidades
+
+  const close = () => {
+    window.location.href = "/";
+  }
   return (
-    <div>
-      <h1>Pagina Configurações</h1>
+    <div className="usuarios-container">
+      <Modal show={true} onClose={close}>
+        <Modal.Title>Em Breve</Modal.Title>
+        <Modal.Content>
+          <Typography variant={"span"}>Em breve você poderá alterar a quantidade de dias que o sistema</Typography>
+          <Typography variant={"span"}>considera para marcar um cliente como inadimplente.</Typography>
+          <Button onClick={close}>OK</Button>
+        </Modal.Content>
+      </Modal>
+      <Typography variant={"h3"}>Configurações</Typography>
+      <div>
+
+        <Input outerClassname="usuarios-w400" label={'Dias para ficar inadimplente'} placeholder={""} />
+        <Button onClick={() => { }}>Salvar</Button>
+      </div>
     </div>
   );
 }
